f05fd589dbf36fa3020c82e2ae6226e6cdde766a
IvanPolienko <piar1986@yandex.ru>
2020-06-15 12:53:45 +0700

    Uploaded places from the database to the template

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse
 from django.template import loader
+from places.models import Place
+
+
+def format_coordinate(coordinate):
+    return str(coordinate).replace(',', '.')
 
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
+    places=Place.objects.all()
+    places_details = []
+    for place in places:
+        place_longitude_formated = format_coordinate(place.lng)
+        place_latitude_formated = format_coordinate(place.lat)
+        places_details.append({
+            'title': place.title,
+            'longitude': place_longitude_formated,
+            'latitude': place_latitude_formated,
+            'placeId': 'moscow_legends',
+            'detailsUrl': 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json'
+            })
+    context = {
+        'places':places_details
+        }
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
